45b10e422f267978ae15e037226a24ac610eb083
davidfraj <davidfraj@gmail.com>
2015-03-18 12:11:51 +0100

    insertargal.php
    Insercion de imagenes en la galeria

---
+++ inserciongal.php
@@ -0,0 +1,24 @@
+<header>Alta de imagen realizada</header>
+
+<?php
+//Recoger los datos de la pagina insertar.php
+$descripcion_ima=$_POST['descripcion_ima'];
+$archivo_ima=$_FILES['archivo_ima']['name'];
+
+//Como hay una imagen, lo subimos a su sitio
+move_uploaded_file($_FILES['archivo_ima']['tmp_name'], 'galerias/'.$archivo_ima);
+
+//Tenemos que insertar los datos en la BBDD
+$sql="INSERT INTO imagenes(descripcion_ima, archivo_ima) VALUES ('$descripcion_ima', '$archivo_ima')";
+$consulta=$conexion->query($sql);
+
+//Segun como han ido las cosas, mostramos un mensaje al usuario
+if($consulta==true){
+	echo 'Insertado con exito';
+	//Esto es para volver automaticamente al index
+	header('location:index.php?p=galeria.php');
+}else{
+	echo 'Ha habido un problema al insertar';
+}
+
+?>
---
+++ insertargal.php
@@ -0,0 +1,19 @@
+<header>Alta de imagen</header>
+
+<form method="post" action="index.php?p=inserciongal.php" enctype="multipart/form-data">
+
+	<fieldset>
+		<legend>Rellena los campos</legend>
+
+		<span>Imagen</span>
+		<input type="file" name="archivo_ima">
+		<br>
+
+		<span>Descripcion</span>
+		<textarea name="descripcion_ima"></textarea>
+		<br>
+
+		<input type="submit" name="enviar" value="Enviar">
+	</fieldset>
+	
+</form>
