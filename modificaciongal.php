0948b49285621f3253152a27844556b3a3abb4e7
davidfraj <davidfraj@gmail.com>
2015-03-18 12:47:37 +0100

    modificargal.php
    Modificacion de las imagenes

---
+++ modificaciongal.php
@@ -0,0 +1,36 @@
+<header>Modificacion de imagen</header>
+
+<?php
+//Recoger los datos de la pagina modificar
+$archivo_ima_actual=$_POST['archivo_ima_actual'];
+$descripcion_ima=$_POST['descripcion_ima'];
+$id_ima=$_POST['id_ima'];
+
+$archivo_ima=$_FILES['archivo_ima']['name'];
+
+//Subir la imagen si fuera necesario
+if(is_uploaded_file($_FILES['archivo_ima']['tmp_name'])){
+	//Borro la anterior
+	unlink('galerias/'.$archivo_ima_actual);
+	//Pongo en su sitio la nueva
+	move_uploaded_file($_FILES['archivo_ima']['tmp_name'], 'galerias/'.$archivo_ima);
+}else{
+	$archivo_ima=$archivo_ima_actual;
+}
+
+//Establecer consulta de modificacion
+$sql="UPDATE imagenes SET archivo_ima='$archivo_ima', descripcion_ima='$descripcion_ima' WHERE id_ima=$id_ima";
+
+//Ejecutamos la consulta
+$consulta=$conexion->query($sql);
+
+//mostramos un mensaje al usuario
+if($consulta==true){
+	echo 'Modificado con exito';
+	//Esto es para volver automaticamente al index
+	header('location:index.php?p=galeria.php');
+}else{
+	echo 'Ha habido un problema al modificar';
+}
+
+?>
---
+++ modificargal.php
@@ -0,0 +1,31 @@
+<?php
+//Recojo el id de imagen
+$id_ima=$_GET['id_ima'];
+//Hago una consulta a la bbdd sobre esa imagen
+$sql="SELECT * FROM imagenes WHERE id_ima=$id_ima";
+//Ejecuto la consulta
+$consulta=$conexion->query($sql);
+//Extraigo en $fila, la UNICA imagen que coincide con $id_ima
+$fila=$consulta->fetch_array();
+?>
+
+<header>Modificar imagen</header>
+<form method="post" action="index.php?p=modificaciongal.php" enctype="multipart/form-data">
+	<fieldset>
+		<legend>Rellena los campos</legend>
+
+		<span>Imagen</span>
+		<input type="file" name="archivo_ima">
+		<img src="galerias/<?php echo $fila['archivo_ima'];?>" width="100">
+		<input type="hidden" name="archivo_ima_actual" value="<?php echo $fila['archivo_ima'];?>">
+		<br>
+
+		<span>Descripcion imagen</span>
+		<textarea name="descripcion_ima"><?php echo $fila['descripcion_ima'];?></textarea>
+		<br>
+
+		<input type="hidden" name="id_ima" value="<?php echo $fila['id_ima'];?>">
+
+		<input type="submit" name="enviar" value="Enviar">
+	</fieldset>
+</form>
